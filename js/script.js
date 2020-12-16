9762daf08491803e567b11b5fe98c778f379fa71
Dima Afanasev <duma12let@gmail.com>
2020-12-16 13:03:16 +0200

    fix mobile scroll

--- js/script.js
+++ js/script.js
@@ -4,48 +4,55 @@ let h_b = document.querySelector('.parallax').clientHeight;
 let label = document.querySelector('.label');
 let labelStartPositionX = parseInt(getComputedStyle(label).left);
 
+let offset = 0;
+if (window.matchMedia("(max-width: 760px)").matches) {
+	offset = 300;
+}
+
 document.addEventListener('DOMContentLoaded', function () {
 	window.addEventListener('scroll', function () {
 		//console.log(window.outerWidth+ " "+ window.outerHeight);
 		let s = window.pageYOffset;
-		
-		let p = s / h * 100;
-		let p_b = s / h_b * 100;
-		let o = 1 - 1 / 100 * p_b;
+		if (s > offset) {
+			s-=offset;
+			let p = s / h * 100;
+			let p_b = s / h_b * 100;
+			let o = 1 - 1 / 100 * p_b;
 
-		label.style.left = (labelStartPositionX + Math.pow(s/100, 3)) +'px';
+			label.style.left = (labelStartPositionX + Math.pow(s / 100, 3)) + 'px';
 
-		let z_1 = 1 + (w / 10000 * p_b);
-		document.querySelector('.parallax__snowfall-first').style.transform = `scale(${z_1})`;
-		document.querySelector('.parallax__snowfall-first').style.opacity = Math.abs(o);
-		
-		z_1 = 1 + (w / 1000000 * p_b);
-		document.querySelector('.parallax__snowfall-second').style.transform = `scale(${z_1})`;
-		document.querySelector('.parallax__snowfall-second').style.opacity = Math.abs(o);
+			let z_1 = 1 + (w / 10000 * p_b);
+			document.querySelector('.parallax__snowfall-first').style.transform = `scale(${z_1})`;
+			document.querySelector('.parallax__snowfall-first').style.opacity = Math.abs(o);
+
+			z_1 = 1 + (w / 1000000 * p_b);
+			document.querySelector('.parallax__snowfall-second').style.transform = `scale(${z_1})`;
+			document.querySelector('.parallax__snowfall-second').style.opacity = Math.abs(o);
 
 
-		z_1 = 1 + (w / 5000000 * p);
-		document.querySelector('.parallax__background').style.transform = `scale(${z_1})`;
+			z_1 = 1 + (w / 5000000 * p);
+			document.querySelector('.parallax__background').style.transform = `scale(${z_1})`;
 
-		let hr = w / 2000 * p_b;
-		z_1 = 1 + (w * 0.000005 * p_b);
-		document.querySelector('.parallax__snow-main').style.transform = `translate3d(${hr}px,0,0) scale(${z_1})`;
+			let hr = w / 2000 * p_b;
+			z_1 = 1 + (w * 0.000005 * p_b);
+			document.querySelector('.parallax__snow-main').style.transform = `translate3d(${hr}px,0,0) scale(${z_1})`;
 
-		hr = w / 1500 * p_b;
-		z_1 = 1 + (w * 0.00001 * p_b);
-		document.querySelector('.parallax__tree').style.transform = `translate3d(${hr}px,0,0) scale(${z_1})`;
+			hr = w / 1500 * p_b;
+			z_1 = 1 + (w * 0.00001 * p_b);
+			document.querySelector('.parallax__tree').style.transform = `translate3d(${hr}px,0,0) scale(${z_1})`;
 
-		//let hr_3 = w / 1500 * p_b;
-		//let z_5 = 1 + (w * 0.00001 * p_b); 
-		document.querySelector('.parallax__snow-near-tree').style.transform = `translate3d(${hr}px,0,0) scale(${z_1})`;
+			//let hr_3 = w / 1500 * p_b;
+			//let z_5 = 1 + (w * 0.00001 * p_b); 
+			document.querySelector('.parallax__snow-near-tree').style.transform = `translate3d(${hr}px,0,0) scale(${z_1})`;
 
-		// let hr_4 = w / 1500 * p_b;
-		//let z_6 = 1 + (w * 0.00001 * p_b); 
-		document.querySelector('.parallax__gift').style.transform = `translate3d(${hr}px,0,0) scale(${z_1})`;
+			// let hr_4 = w / 1500 * p_b;
+			//let z_6 = 1 + (w * 0.00001 * p_b); 
+			document.querySelector('.parallax__gift').style.transform = `translate3d(${hr}px,0,0) scale(${z_1})`;
 
-		// let hr_5 = w / 1500 * p_b;
-		//let z_7 = 1 + (w * 0.00001 * p_b); 
-		document.querySelector('.parallax__snowman').style.transform = `translate3d(${hr}px,0,0) scale(${z_1})`; 
+			// let hr_5 = w / 1500 * p_b;
+			//let z_7 = 1 + (w * 0.00001 * p_b); 
+			document.querySelector('.parallax__snowman').style.transform = `translate3d(${hr}px,0,0) scale(${z_1})`;
+		}
 	});
 
 });;
